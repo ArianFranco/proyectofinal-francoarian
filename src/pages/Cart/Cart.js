e93381335b35f0ba9977ba58b971f14854e031d7
Arian Franco <ariifre2@gmail.com>
2023-03-09 20:56:04 -0300

    reentrega

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,5 +1,16 @@
+import { useContext } from "react"
+import { cartContext } from "../../context/cartContext"
+
 const Cart = () => {
-    return <div>Cart</div>
+    const {cart} = useContext(cartContext)
+    console.log(cart)
+    return (
+    <div>
+        {cart.map((product) => (
+            <h1 key ={product.id}>{product.title}</h1>
+        ))}
+    </div>
+    )
 }
 
 export default Cart
